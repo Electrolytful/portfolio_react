bdd2beedc1550beb19b009ad1485a6be6438433d
Electrolytful <erhanulusal@hotmail.com>
2023-08-30 17:27:11 +0100

    added parallax effect and fade in to hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import {
   Hero,
+  About,
 } from "./pages";
 
 export default function App() {
   return (
-    <>
+    <div className="wrapper">
       <Hero />
-    </>
+      <About />
+    </div>
   );
 }
--- src/pages/Hero/index.jsx
+++ src/pages/Hero/index.jsx
@@ -1,14 +1,21 @@
 import styles from "./index.module.scss";
+import { useState, useEffect } from "react";
 
 export default function Hero() {
+  const [contentLoaded, setContentLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {setContentLoaded(true)}, 100);
+  }, []);
+
   return (
     <header className={styles.hero}>
-      <div className={styles.hero__background}></div>
-      <div className={styles.hero__content}>
+      <div className={styles.hero_background}></div>
+      <div className={`${styles.hero_content} ${contentLoaded ? styles.hero_content___visible : ''}`}>
         <img src="images/hero_picture.jpg" alt="Picture of me" />
         <h1>Erhan Ulusal</h1>
         <h2>Web Developer</h2>
-        <ul className={styles.hero__icons}>
+        <ul className={styles.hero_icons}>
           <li onClick={() => window.open("https://twitter.com/electrolytful", "_blank")}>
             <i className="fa-brands fa-twitter"></i>
           </li>
